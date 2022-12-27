368d25f879350b82025f80755e3bc0c6a3e4c7b6
ygor xavier de sousa silva <68355711+YgorXavierS@users.noreply.github.com>
2022-12-27 13:55:25 -0300

    add exercício 3

---
+++ FUNÇÕES/3.py
@@ -0,0 +1,9 @@
+'''
+
+Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos. 
+'''
+
+def calc(item1,item2,item3):
+    return (item1+item2+item3)
+
+print(calc(2,2,3))
